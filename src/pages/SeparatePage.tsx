402f1dad973c99b4ca0ad76323a4728f31c35039
VolodymyrBoychuk <vladimirboychuk@gmail.com>
2025-04-25 17:08:49 +0300

    Blog
    added css and move post to separate file

--- src/pages/SeparatePage.tsx
+++ src/pages/SeparatePage.tsx
@@ -1,93 +1,30 @@
-import { Box } from '@chakra-ui/react';
+import { Box, VStack, Text } from '@chakra-ui/react';
 import MarkdownRenderer from '../components/MarkdownRenderer';
+import { markdownContent } from '../data/content';
 
 const SeparatePage = () => {
-  // Example markdown content
-  // This can be replaced with any markdown content you want to render
-  // https://markdownlivepreview.com/
-  const markdownContent = `
-# Markdown syntax guide
-
-## Headers
-
-# This is a Heading h1
-## This is a Heading h2
-###### This is a Heading h6
-
-## Emphasis
-
-*This text will be italic*  
-_This will also be italic_
-
-**This text will be bold**  
-__This will also be bold__
-_You **can** combine them_
-
-## Lists
-
-### Unordered
-
-* Item 1
-* Item 2
-* Item 2a
-* Item 2b
-    * Item 3a
-    * Item 3b
-
-### Ordered
-
-// 1. Item 1
-// 2. Item 2
-// 3. Item 3
-//     1. Item 3a
-//     2. Item 3b
-
-## Links
-[This is a link](https://www.google.com)
-
-## Blockquotes
-> This is a blockquote
-> This is a blockquote
-
-## Tables
-| Tables        | Are           | Cool  |
-| ------------- |:-------------:| -----:|
-| col 3 is      | right-aligned | \$1600 |
-| col 2 is      | centered      |   \$12 |
-| col 1 is      | left-aligned  |    \$1 |
-
-## Block of code
-\`\`\`swift
-class Person {
-    var name: String
-    var age: Int
-
-    init(name: String, age: Int) {
-        self.name = name
-        self.age = age
-    }
-
-    func greet() {
-        print("Привіт, мене звати \(name) і мені \(age) років.")
-    }
-}
-
-let person = Person(name: "Оля", age: 25)
-person.greet()
-\`\`\`
-## Inline code
-This is \`inline code\`
-
-## Images
-![This is an image](https://deutschvektor.com/images/blog/image_describe_b1.jpg "Image Title")
-![This is an image](https://deutschvektor.com/images/blog/image_describe_b1.jpg "Image Title")
-
-  `;
-
   return (
-    <Box p={4}>
-      <MarkdownRenderer content={markdownContent} />
-    </Box>
+    <>
+      <Box p={4} ml={200} mr={200} minH="100vh">
+        <Text fontSize="4xl" mb={4} textAlign="center">
+          Latest posts
+        </Text>
+        <VStack spacing={6} align="stretch" bg={'#4a90e2'} p={4} borderRadius="md" boxShadow="md">
+          {markdownContent.map((content, index) => (
+            <Box
+              key={index}
+              p={6}
+              borderRadius="md"
+              boxShadow="md"
+              border="1px solid"
+              borderColor="gray.200"
+            >
+              <MarkdownRenderer content={content} />
+            </Box>
+          ))}
+        </VStack>
+      </Box>
+    </>
   );
 };
 
